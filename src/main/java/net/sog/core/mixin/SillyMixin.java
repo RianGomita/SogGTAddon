2637ea98d0b5b2ec7bdb51cb643227dd41efd817
Connor Smith <phoenixvine32908@gmail.com>
2025-09-24 13:06:04 -0400

    Initial

--- src/main/java/net/sog/core/mixin/SillyMixin.java
+++ src/main/java/net/sog/core/mixin/SillyMixin.java
@@ -1,12 +1,10 @@
 package net.sog.core.mixin;
 
 
-import org.spongepowered.asm.mixin.Mixin;
-
 // This is a dummy mixin! It doesn't actually do anything.
 // Mixins are ways to modify code in other classes.
 // They can be very valuable in the right circumstances,
 // but it is generally preferred that you try to use other means
 // to get your code to work before resorting to mixins,
 // as they can be highly invasive.
-public class DummyMixin {}
+public class SillyMixin {}
